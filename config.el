523b05cf674e1e1fb43deb7bbe7fd71ce6ab3add
Sean Bergstedt <s.bergstedt@engusa.com>
2020-08-24 18:27:00 -0500

    finish adding all .emacs lisp settings to config file

--- config.el
+++ config.el
@@ -1,5 +1,15 @@
 (setq inhibit-startup-screen t)
 
+(setq visible-bell 1)
+
+(global-linum-mode t)
+
+(setq initial-frame-alist '(
+            (top . 40) (left . 100)
+            (width . 128) (height . 50)
+            )
+      )
+
 ;; --------------------------------------------------
 ;; Setup custom variables
 ;; --------------------------------------------------
@@ -30,3 +40,158 @@
 (add-to-list `load-path "~/.emacs.d/lisp/")
 
 (load "stm-mode")
+
+(add-to-list 'load-path "~/.emacs.d/vendor/alchemist.el/")
+(require 'alchemist)
+
+;; ;; add the location of the elisp files to the load-path
+;; (setq load-path (cons  "/usr/lib/erlang/lib/tools-2.6.13/emacs"
+;;          load-path))
+;; ;; set the location of the man page hierarchy
+;; (setq erlang-root-dir "/usr/lib/erlang")
+;; ;; add the home of the erlang binaries to the exec-path
+;; (setq exec-path (cons "/usr/lib/bin" exec-path))
+;; ;; load and eval the erlang-start package to set up 
+;; ;; everything else 
+;; (require 'erlang-start)
+
+(setq inferior-ess-r-program "R")
+(add-hook 'ess-mode-hook
+          (lambda () 
+            (ess-toggle-underscore nil)))
+
+(add-hook 'java-mode-hook (lambda ()
+			    (setq c-basic-offset 2
+				  tab-width 2
+				  indent-tabs-mode t)))
+
+;;(load (expand-file-name "~/quicklisp/slime-helper.el"))
+;; Replace "sbcl" with the path to your implementation
+;;(setq inferior-lisp-program "sbcl")
+
+(add-to-list 'auto-mode-alist '("\\.stm\\'" . mystm-mode))
+(when (fboundp 'mystm-mode)
+
+  (defun my-insert-tab-char ()
+    "Insert a tab char. (ASCII 9, \t)"
+    (interactive)
+    (insert "\t"))
+
+  (defun my-tab-config ()
+    ;; setup tab char behavior
+    (local-set-key (kbd "TAB") 'my-insert-tab-char)  
+    )
+  
+  (add-hook 'mystm-mode-hook 'my-tab-config)
+  )
+
+(defun comment-line-break (&optional arg)
+  "Add dashed line break comment"
+  (interactive "*P")
+  (comment-normalize-vars)
+  (if (and (not (region-active-p)) (not (looking-at "[ \t]*$")))
+      (comment-or-uncomment-region (line-beginning-position) (line-end-position))
+    (comment-dwim arg))
+  (insert-char ?- 50))
+  ;; (insert-char ? 20))
+(global-set-key (kbd "C-M-;") `comment-line-break)
+
+(defun bjm-comment-box (b e)
+  "draw a box comment around the region but arrange for the region to extend to at least the fill column.
+place the point after the comment box."
+  (interactive "r")
+  (let ((e (copy-marker e t)))
+    (goto-char b)
+    (end-of-line)
+    (insert-char ? (- fill-column (current-column)))
+    (comment-box b e 1)
+    (goto-char e)
+    (set-marker e nil)))
+;; create comment box
+(global-set-key (kbd "C-c b b") `bjm-comment-box)
+
+(defun move-text-internal (arg)
+  (cond
+   ((and mark-active transient-mark-mode)
+    (if (> (point) (mark))
+        (exchange-point-and-mark))
+    (let ((column (current-column))
+          (text (delete-and-extract-region (point) (mark))))
+      (forward-line arg)
+      (move-to-column column t)
+      (set-mark (point))
+      (insert text)
+      (exchange-point-and-mark)
+      (setq deactivate-mark nil)))
+   (t
+    (let ((column (current-column)))
+      (beginning-of-line)
+      (when (or (> arg 0) (not (bobp)))
+        (forward-line)
+        (when (or (< arg 0) (not (eobp)))
+          (transpose-lines arg)
+          (when (and (eval-when-compile
+                       '(and (>= emacs-major-version 24)
+                             (>= emacs-minor-version 3)))
+                     (< arg 0))
+            (forward-line -1)))
+        (forward-line -1))
+      (move-to-column column t)))))
+
+(defun move-text-down (arg)
+  "Move region (transient-mark-mode active) or current line
+  arg lines down."
+  (interactive "*p")
+  (move-text-internal arg))
+(global-set-key [M-S-down] 'move-text-down)
+
+(defun move-text-up (arg)
+  "Move region (transient-mark-mode active) or current line
+  arg lines up."
+  (interactive "*p")
+  (move-text-internal (- arg)))
+(global-set-key [M-S-up] 'move-text-up)
+
+;; Enable Org mode
+(require 'org)
+;; Make Org mode work with files ending in .org
+;; (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+;; The above is the default in recent emacs
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
+(add-hook 'org-mode-hook #'visual-line-mode) ;line wrap
+(setq org-hide-leading-stars t)
+
+(setq org-agenda-files (append
+			(file-expand-wildcards "~/org/*org")))
+
+(setq org-html-htmlize-output-type 'css)
+
+(require 'ox-twbs)
+
+(require 'ox-latex)
+(unless (boundp 'org-latex-classes)
+  (setq org-latex-classes nil))
+;; (add-to-list 'org-latex-classes
+;;              '("article"
+;;                "\\documentclass{article}"
+;;                ("\\section{%s}" . "\\section*{%s}")))
+(add-to-list 'org-latex-classes
+             '("article"
+               "\\documentclass{article}"
+               ("\\section{%s}" . "\\section*{%s}")
+               ("\\subsection{%s}" . "\\subsection*{%s}")
+               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+               ("\\paragraph{%s}" . "\\paragraph*{%s}")
+               ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+(add-to-list 'org-latex-classes
+             '("book"
+               "\\documentclass{book}"
+               ("\\part{%s}" . "\\part*{%s}")
+               ("\\chapter{%s}" . "\\chapter*{%s}")
+               ("\\section{%s}" . "\\section*{%s}")
+               ("\\subsection{%s}" . "\\subsection*{%s}")
+               ("\\subsubsection{%s}" . "\\subsubsection*{%s}"))
+             )
+(put 'downcase-region 'disabled nil)
